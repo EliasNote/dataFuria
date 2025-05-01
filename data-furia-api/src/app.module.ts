4a86d8fa21d97ec5101710a078c8b63f32e5022c
Elias Mathias Sand <elias.coder1@gmail.com>
2025-05-01 15:14:57 -0300

    feat: Configuração do app.module

--- data-furia-api/src/app.module.ts
+++ data-furia-api/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { SocialAccountsModule } from './social-accounts/social-accounts.module';
+import { FileUploadModule } from './file-upload/file-upload.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UsersModule,
+    FileUploadModule,
+    SocialAccountsModule,
+  ],
+  providers: [],
 })
 export class AppModule {}
