eeb2cf22b323b8f1e59fd18b86b6ecd4edb8730a
Elias Mathias Sand <elias.coder1@gmail.com>
2025-05-01 14:58:42 -0300

    config

--- data-furia-api/eslint.config.mjs
+++ data-furia-api/eslint.config.mjs
@@ -19,9 +19,11 @@ export default tseslint.config(
       },
       ecmaVersion: 5,
       sourceType: 'module',
+      parser: '@typescript-eslint/parser',
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
+        project: ['./tsconfig.json'],
       },
     },
   },
@@ -29,7 +31,7 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
     },
   },
 );
