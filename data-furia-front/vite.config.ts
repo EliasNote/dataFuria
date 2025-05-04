54ad2dd2ebf4c07cd9588d4b5241687dda33607c
Elias Mathias Sand <elias.coder1@gmail.com>
2025-05-04 19:24:09 -0300

    fix: Update base path in vite.config.ts for correct deployment

--- data-furia-front/vite.config.ts
+++ data-furia-front/vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-	base: "/data-furia-front/",
+	base: "./",
+
 	plugins: [react()],
 	build: {
 		outDir: "docs",
